54afbe8afd68240c013c03b59c8fead22b13a975
Victor Hugo Albuquerque <vh12albuuerque@gmail.com>
2021-06-17 11:07:26 -0300

    finalizando API(order)

--- food-app-api/db/schema.rb
+++ food-app-api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_16_170001) do
+ActiveRecord::Schema.define(version: 2021_06_17_131909) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 2021_06_16_170001) do
     t.integer "restaurant_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "street"
     t.index ["restaurant_id"], name: "index_orders_on_restaurant_id"
   end
 
